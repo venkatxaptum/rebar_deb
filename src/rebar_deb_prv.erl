ccfbd22616015b26381f342b150331434d4d1cb9
Venkatakumar Srinivasan <venkat@xaptum.com>
2016-07-26 15:39:06 -0500

    .

--- src/rebar_deb_prv.erl
+++ src/rebar_deb_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, 'deb').
--define(DEPS, [release]).
+-define(DEPS, [compile]).
 
 %% ===================================================================
 %% Public API
